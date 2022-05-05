f94e1c8342f75825a676a048c54076227811d6b2
akshayprojects <42297296+akshaysoni8707@users.noreply.github.com>
2022-05-05 16:54:34 +0530

    AutoCompleteTextView and multiAutoCompleteTextView adapter completed

--- app/src/main/java/com/app/mycalc/FormActivity.java
+++ app/src/main/java/com/app/mycalc/FormActivity.java
@@ -6,7 +6,12 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +24,100 @@ public class FormActivity extends AppCompatActivity {
     EditText password;
     TextView passwordError;
 
+    AutoCompleteTextView selectState;
+    String[] states = {
+            "Andhra Pradesh",
+            "Arunachal Pradesh",
+            "Assam",
+            "Bihar",
+            "Chhattisgarh",
+            "Goa",
+            "Gujarat",
+            "Haryana",
+            "Himachal Pradesh",
+            "Jammu and Kashmir",
+            "Jharkhand",
+            "Karnataka",
+            "Kerala",
+            "Madhya Pradesh",
+            "Maharashtra",
+            "Manipur",
+            "Meghalaya",
+            "Mizoram",
+            "Nagaland",
+            "Odisha",
+            "Punjab",
+            "Rajasthan",
+            "Sikkim",
+            "Tamil Nadu",
+            "Telangana",
+            "Tripura",
+            "Uttarakhand",
+            "Uttar Pradesh",
+            "West Bengal"
+    };
+
+    MultiAutoCompleteTextView selectCities;
+    String[] cities = {
+            "Bangalore",
+            "Chennai",
+            "Delhi",
+            "Hyderabad",
+            "Kolkata",
+            "Mumbai",
+            "Pune",
+            "Ahmedabad",
+            "Aurangabad",
+            "Bangalore",
+            "Belgaum",
+            "Bhubaneswar",
+            "Chandigarh",
+            "Cochin",
+            "Coimbatore",
+            "Cuttack",
+            "Dehradun",
+            "Dhanbad",
+            "Gandhinagar",
+            "Gurgaon",
+            "Guwahati",
+            "Hubli",
+            "Indore",
+            "Jabalpur",
+            "Jaipur",
+            "Jalandhar",
+            "Jammu",
+            "Jamshedpur",
+            "Jhansi",
+            "Kakinada",
+            "Kochi",
+            "Kolkata",
+            "Lucknow",
+            "Ludhiana",
+            "Madurai",
+            "Mangalore",
+            "Meerut",
+            "Nagpur",
+            "Nashik",
+            "Noida",
+            "Patna",
+            "Pimpri-Chinchwad",
+            "Pune",
+            "Raipur",
+            "Rajkot",
+            "Ranchi",
+            "Surat",
+            "Thiruvananthapuram",
+            "Thrissur",
+            "Tiruchirappalli",
+            "Tirunelveli",
+            "Tiruppur",
+            "Ujjain",
+            "Vadodara",
+            "Vijayawada",
+            "Visakhapatnam",
+            "Warangal"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +126,8 @@ public class FormActivity extends AppCompatActivity {
         userNameError = findViewById(R.id.UserNameError);
         password = findViewById(R.id.password);
         passwordError = findViewById(R.id.passwordError);
+        selectState = findViewById(R.id.selectState);
+        selectCities = findViewById(R.id.selectCities);
 
         TextWatcher userNameTextWatcher = new TextWatcher() {
             @Override
@@ -71,12 +172,11 @@ public class FormActivity extends AppCompatActivity {
                     passwordError.setText(R.string.PasswordRequired);
                     passwordError.setTextColor(getColor(R.color.tomato));
                     Toast.makeText(FormActivity.this, "Password is required", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    if(Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,10}$", text)){
+                } else {
+                    if (Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,10}$", text)) {
                         passwordError.setText(R.string.PasswordValid);
                         passwordError.setTextColor(getColor(R.color.teal_700));
-                    }else{
+                    } else {
                         passwordError.setText(R.string.PasswordInvalid);
                         passwordError.setTextColor(getColor(R.color.tomato));
                     }
@@ -84,5 +184,33 @@ public class FormActivity extends AppCompatActivity {
             }
         };
         password.addTextChangedListener(passwordTextWatcher);
+
+        selectState.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, states));
+
+        selectState.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(FormActivity.this, "You selected " + states[i], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(FormActivity.this, "Please select a state", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        selectCities.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, cities));
+        selectCities.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        selectCities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(FormActivity.this, "You selected " + cities[i], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(FormActivity.this, "Please select a city", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
